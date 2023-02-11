cb71ef9ac3c5a4eb4e3cb4a1c1880645261f573c
Даниил <@DESKTOP-NA8A3QK>
2023-02-11 10:35:59 +0700

    Добавьте файлы проекта.

---
+++ transformation/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace transformation
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Sum s = new Sum();
+            Console.WriteLine(s.GetSum(1, 2));
+
+        }
+        public class Sum
+        {
+            public int GetSum(int a, int b)
+            {
+                int sum = 0;
+                if (a == b)
+                    sum = b;
+                else if (a > b)
+                {
+                    for (int i = b; i <= a; i++)
+                        sum += i;
+
+                }
+                else if (a < b)
+                {
+                    for (int i = a; i <= b; i++)
+                        sum += i;
+                }
+                return sum;
+
+            }
+        }
+
+    }
+}
+
+
+
+
+
